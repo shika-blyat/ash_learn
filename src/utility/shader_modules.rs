4ccb9ee833cf56e39c6ade211d557b98c5c990de
shika-blyat <abdelzighem1@gmail.com>
2019-12-30 03:31:17 +0100

    finished shader modules chapter

--- src/utility/shader_modules.rs
+++ src/utility/shader_modules.rs
@@ -2,7 +2,10 @@ use crate::utility::vulkanapp::VulkanApp;
 
 use ash::{
     version::DeviceV1_0,
-    vk::{ShaderModule, ShaderModuleCreateInfo, StructureType},
+    vk::{
+        PipelineShaderStageCreateInfo, ShaderModule, ShaderModuleCreateInfo, ShaderStageFlags,
+        StructureType,
+    },
     Device,
 };
 use std::mem;
@@ -13,6 +16,20 @@ impl VulkanApp {
         let vert_shader = include_bytes!("../shaders/vert.spv");
         let frag_module = VulkanApp::create_shader_module(device, frag_shader.to_vec());
         let vert_module = VulkanApp::create_shader_module(device, vert_shader.to_vec());
+        let vert_shader_stage_create_info = PipelineShaderStageCreateInfo {
+            s_type: StructureType::PIPELINE_SHADER_STAGE_CREATE_INFO,
+            stage: ShaderStageFlags::VERTEX,
+            module: vert_module,
+            p_name: "main".as_ptr() as *const i8,
+            ..Default::default()
+        };
+        let frag_shader_stage_create_info = PipelineShaderStageCreateInfo {
+            s_type: StructureType::PIPELINE_SHADER_STAGE_CREATE_INFO,
+            stage: ShaderStageFlags::FRAGMENT,
+            module: frag_module,
+            p_name: "main".as_ptr() as *const i8,
+            ..Default::default()
+        };
         unsafe {
             device.destroy_shader_module(frag_module, None);
             device.destroy_shader_module(vert_module, None);
